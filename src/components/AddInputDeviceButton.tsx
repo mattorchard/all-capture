32487bec0430b1e60f36703e0338fe3cebb0ed2e
Matthew <morch028@uottawa.ca>
2020-07-26 00:53:33 -0400

    Separates audio and video inputs into sections

--- src/components/AddInputDeviceButton.tsx
+++ src/components/AddInputDeviceButton.tsx
@@ -8,6 +8,8 @@ import {
   Icon,
   Menu,
   MenuButton,
+  MenuDivider,
+  MenuGroup,
   MenuItem,
   MenuList,
 } from "@chakra-ui/core";
@@ -40,42 +42,24 @@ const getConstraintsForDeviceInfo = (
   }
 };
 
-const isInputDevice = (deviceInfo: MediaDeviceInfo) =>
-  deviceInfo.kind.toLowerCase().includes("input");
-
-const InputKindBadge: React.FC<{ kind: string } & BadgeProps> = ({
-  kind,
-  ...props
-}) => {
-  switch (kind.toLowerCase()) {
-    case "audioinput":
-      return (
-        <Badge variantColor="orange" {...props}>
-          Audio
-        </Badge>
-      );
-    case "videoinput":
-      return (
-        <Badge variantColor="teal" {...props}>
-          Video
-        </Badge>
-      );
-    default:
-      return (
-        <Badge variantColor="red" {...props}>
-          Unknown Type
-        </Badge>
-      );
-  }
-};
-
 const AddInputDeviceButton: React.FC<
   { onTracksAdded: (annotatedTracks: AnnotatedTrack[]) => void } & ButtonProps
 > = ({ onTracksAdded, children, ...buttonProps }) => {
   const [mediaDevices, updateMediaDevices] = useMediaDevices();
-  const inputDevices = useMemo(() => mediaDevices?.filter(isInputDevice), [
-    mediaDevices,
-  ]);
+  const { audioInputDevices, videoInputDevices } = useMemo(() => {
+    const audioInputDevices: MediaDeviceInfo[] = [];
+    const videoInputDevices: MediaDeviceInfo[] = [];
+
+    mediaDevices?.forEach((deviceInfo) => {
+      if (deviceInfo.kind.toLowerCase() === "audioinput") {
+        audioInputDevices.push(deviceInfo);
+      } else if (deviceInfo.kind.toLowerCase() === "videoinput") {
+        videoInputDevices.push(deviceInfo);
+      }
+    });
+
+    return { audioInputDevices, videoInputDevices };
+  }, [mediaDevices]);
 
   const getTracksFromDevice = async (deviceInfo: MediaDeviceInfo) => {
     const stream = await navigator.mediaDevices.getUserMedia(
@@ -92,14 +76,13 @@ const AddInputDeviceButton: React.FC<
     onTracksAdded(annotatedTracks);
   };
 
-  const noInputsYet = !inputDevices || inputDevices.length === 0;
   return (
     <Menu>
       <MenuButton
         as={Button}
         {...buttonProps}
         // @ts-ignore
-        isDisabled={noInputsYet || buttonProps.isDisabled}
+        isDisabled={buttonProps.isDisabled}
         leftIcon="add"
         rightIcon="chevron-down"
       >
@@ -107,17 +90,34 @@ const AddInputDeviceButton: React.FC<
       </MenuButton>
 
       <MenuList>
-        {noInputsYet || (
+        {videoInputDevices.length > 0 && (
           <>
-            {inputDevices!.map((deviceInfo, index) => (
-              <MenuItem
-                onClick={() => getTracksFromDevice(deviceInfo)}
-                key={deviceInfo.deviceId || deviceInfo.groupId || index}
-              >
-                {deviceInfo.label || "Unlabeled Device"}{" "}
-                <InputKindBadge kind={deviceInfo.kind} ml={2} />
-              </MenuItem>
-            ))}
+            <MenuGroup title="Video">
+              {videoInputDevices!.map((deviceInfo, index) => (
+                <MenuItem
+                  onClick={() => getTracksFromDevice(deviceInfo)}
+                  key={deviceInfo.deviceId || deviceInfo.groupId || index}
+                >
+                  {deviceInfo.label || "Unlabeled Camera"}
+                </MenuItem>
+              ))}
+            </MenuGroup>
+            <MenuDivider />
+          </>
+        )}
+        {audioInputDevices.length > 0 && (
+          <>
+            <MenuGroup title="Audio">
+              {audioInputDevices!.map((deviceInfo, index) => (
+                <MenuItem
+                  onClick={() => getTracksFromDevice(deviceInfo)}
+                  key={deviceInfo.deviceId || deviceInfo.groupId || index}
+                >
+                  {deviceInfo.label || "Unlabeled Microphone"}
+                </MenuItem>
+              ))}
+            </MenuGroup>
+            <MenuDivider />
           </>
         )}
         <MenuItem onClick={updateMediaDevices}>
